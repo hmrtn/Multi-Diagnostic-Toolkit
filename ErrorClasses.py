29601af56d7cc9c7bf7c1c29d72f6f9009df8d1d
KaitoDurkee <kaitodurkee@gmail.com>
2019-03-28 09:46:58 -0700

    Added new error class and updated documentation

--- ErrorClasses.py
+++ ErrorClasses.py
@@ -1,9 +1,27 @@
+"""Error Classes Module
+
+This module contains the author-defined error classes used elsewhere in the
+program.
+"""
+
+__author__ = 'Kaito Durkee'
+
 class Error(Exception):
     """Base class for exceptions in this module."""
     pass
 
 class NotImplementedError(Error):
     """Exception raised due to feature not yet being implemented.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+class FileError(Error):
+    """Exception raised due to file not being found.
 
     Attributes:
         message -- explanation of the error
